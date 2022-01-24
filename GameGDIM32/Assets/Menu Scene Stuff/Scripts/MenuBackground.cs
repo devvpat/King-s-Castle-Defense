5bbe12c8150b534eb30fe92c40e73fded90d1dab
tienyil <97574190+tienyil@users.noreply.github.com>
2022-01-23 23:02:46 -0800

    Main Menu Update
    Game Main Menu Setup

---
+++ GameGDIM32/Assets/Menu Scene Stuff/Scripts/MenuBackground.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuBackground : MonoBehaviour
+{
+
+    float scrollSpeed = -80f;
+    Vector2 startPos;
+
+    [SerializeField]
+    public int length;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newPos = Mathf.Repeat(Time.time * scrollSpeed, length);
+        transform.position = startPos + Vector2.left * newPos;
+    }
+}
